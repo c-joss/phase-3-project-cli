4395fd28548fb0fcbe90f5eca2d33c278e7b14ac
Courtney Macgregor <cjoss90@hotmail.com>
2025-06-16 10:03:07 +1000

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import questionary
+
+
+def main_menu():
+    while true:
+        choice = questionary.select(
+            "What would you like to do?",
+            choices=[
+                "Add Rate",
+                "View Rates",
+                "Edit Rates",
+                "Delete Rate",
+                "Export Quote",
+                "Exit",
+            ],
+        ).ask()
+
+        if choice == "Add Rate":
+            add_rate()
+        elif choice == "View Rates":
+            view_rates()
+        elif choice == "Edit Rates":
+            edit_rates()
+        elif choice == "Delete Rate":
+            delete_rate()
+        elif choice == "Export Quote":
+            export_quote()
+        elif choice == "Exit":
+            print("Exiting Rate Management App")
+            break
